6673613bb4f1cc8db715255b20ab44cd1c818cad
Sean Finegan <sean.j.finegan@gmail.com>
2021-04-12 00:19:09 -0700

    updated js, added html and css

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-var content = document.getElementsByClassName("background")[0];
-var title = document.querySelector("h1");
+// hook on dom loaded event
+window.addEventListener('DOMContentLoaded', (event) => {
+    
+});
+setup();
 
-title.onclick = function(){
-    if(content.className == "background") {
-        content.className = "background2";
-    } else {
-        content.className = "background";
+var App = {
+    Assets: {
+        Images: {
+            Background1: 'url("./assets/stephen-leonardi-YVcg27FuJzs-unsplash.jpg")',
+            Background2: 'url("./assets/keith-misner-h0Vxgz5tyXA-unsplash.jpg")'
+        },
+        BackgroundClass: ['background-common'],
+        Icons: {}
+    },
+    Email: ''
+}
+
+function setup(){
+    document.querySelector("h1").onclick = function(){
+        var content = document.getElementsByClassName(App.Assets.BackgroundClass)[0];
         
+        if (content.style.backgroundImage == App.Assets.Images.Background1) {
+            content.style.backgroundImage = App.Assets.Images.Background2;
+        } else {
+            content.style.backgroundImage = App.Assets.Images.Background1;
+        }
     }
 }
 
